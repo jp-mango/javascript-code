5c01bef9a63822e2e82c5aa9b978422eefe5eb32
Justin <menziesjustin@ymail.com>
2023-11-03 15:53:28 -0400

    - added a guess the number game.

---
+++ Guess The Number/script.js
@@ -0,0 +1,77 @@
+'use strict';
+
+let secretNumber = getRandomNumber();
+let score = 20;
+let highScore = 0;
+
+function updateMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+function updateScore(newScore) {
+  score = newScore;
+  document.querySelector('.score').textContent = score;
+}
+
+function getRandomNumber() {
+  return Math.trunc(Math.random() * 20) + 1;
+}
+
+function updateStylesForCorrectGuess() {
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  document.querySelector('.number').style.width = '30rem';
+}
+
+function resetStyles() {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+}
+
+function displaySecretNumber() {
+  document.querySelector('.number').textContent = secretNumber;
+}
+
+function updateHighScore() {
+  if (score > highScore) {
+    highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
+  }
+}
+
+const clickCheck = function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    updateMessage('No number!');
+  } else if (guess === secretNumber) {
+    updateMessage('You guessed correctly!');
+    displaySecretNumber();
+    updateStylesForCorrectGuess();
+    updateHighScore();
+  } else {
+    updateMessage(
+      `Too ${guess < secretNumber ? 'low' : 'high'}, guess ${
+        guess < secretNumber ? 'higher' : 'lower'
+      }`
+    );
+    if (score > 1) {
+      updateScore(score - 1);
+    } else {
+      updateMessage('YOU LOSE');
+      updateScore(0);
+    }
+  }
+};
+
+const clickAgain = function () {
+  secretNumber = getRandomNumber();
+  updateScore(20);
+  updateMessage('Start guessing...');
+  resetStyles();
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+};
+
+// Event listeners
+document.querySelector('.check').addEventListener('click', clickCheck);
+document.querySelector('.again').addEventListener('click', clickAgain);
