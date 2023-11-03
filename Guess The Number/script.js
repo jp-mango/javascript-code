39d072efcb5032b975b6ba5c3c0a990c8fa6cf0c
Justin <menziesjustin@ymail.com>
2023-11-03 17:53:51 -0400

    - added red screen on loss

--- Guess The Number/script.js
+++ Guess The Number/script.js
@@ -1,75 +1,82 @@
 'use strict';
 
 let secretNumber = getRandomNumber();
-let score = 20;
+let score = 10;
 let highScore = 0;
 
 function updateMessage(message) {
-  document.querySelector('.message').textContent = message;
+	document.querySelector('.message').textContent = message;
 }
 
 function updateScore(newScore) {
-  score = newScore;
-  document.querySelector('.score').textContent = score;
+	score = newScore;
+	document.querySelector('.score').textContent = score;
 }
 
 function getRandomNumber() {
-  return Math.trunc(Math.random() * 20) + 1;
+	return Math.trunc(Math.random() * 10) + 1;
 }
 
 function updateStylesForCorrectGuess() {
-  document.querySelector('body').style.backgroundColor = '#60b347';
-  document.querySelector('.number').style.width = '30rem';
+	document.querySelector('body').style.backgroundColor = '#60b347';
+	document.querySelector('.number').style.width = '30rem';
 }
 
 function resetStyles() {
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+	document.querySelector('body').style.backgroundColor = '#222';
+	document.querySelector('.number').style.width = '15rem';
 }
 
 function displaySecretNumber() {
-  document.querySelector('.number').textContent = secretNumber;
+	document.querySelector('.number').textContent = secretNumber;
 }
 
 function updateHighScore() {
-  if (score > highScore) {
-    highScore = score;
-    document.querySelector('.highscore').textContent = highScore;
-  }
+	if (score > highScore) {
+		highScore = score;
+		document.querySelector('.highscore').textContent = highScore;
+	}
+}
+
+function incorrectGuess() {
+	document.querySelector('body').style.backgroundColor = '#ff0000';
+	document.querySelector('.number').style.width = '30rem';
+	document.querySelector('.number').textContent = `X`;
 }
 
 const clickCheck = function () {
-  const guess = Number(document.querySelector('.guess').value);
+	const guess = Number(document.querySelector('.guess').value);
 
-  if (!guess) {
-    updateMessage('No number!');
-  } else if (guess === secretNumber) {
-    updateMessage('You guessed correctly!');
-    displaySecretNumber();
-    updateStylesForCorrectGuess();
-    updateHighScore();
-  } else {
-    updateMessage(
-      `Too ${guess < secretNumber ? 'low' : 'high'}, guess ${
-        guess < secretNumber ? 'higher' : 'lower'
-      }`
-    );
-    if (score > 1) {
-      updateScore(score - 1);
-    } else {
-      updateMessage('YOU LOSE');
-      updateScore(0);
-    }
-  }
+	if (!guess) {
+		updateMessage('No number!');
+	} else if (guess === secretNumber) {
+		updateMessage('You guessed correctly!');
+		displaySecretNumber();
+		updateStylesForCorrectGuess();
+		updateHighScore();
+	} else {
+		updateMessage(
+			`Too ${guess < secretNumber ? 'low' : 'high'}, guess ${
+				guess < secretNumber ? 'higher' : 'lower'
+			}`
+		);
+		if (score > 1) {
+			updateScore(score - 1);
+		} else {
+			updateMessage('YOU LOSE');
+			updateScore(0);
+			incorrectGuess();
+		}
+	}
 };
 
 const clickAgain = function () {
-  secretNumber = getRandomNumber();
-  updateScore(20);
-  updateMessage('Start guessing...');
-  resetStyles();
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
+	secretNumber = getRandomNumber();
+	updateScore(10);
+	updateMessage('Start guessing...');
+	resetStyles();
+	document.querySelector('.number').textContent = '?';
+	document.querySelector('.guess').value = '';
 };
 
 // Event listeners
